244c83fc11d622367a4af5dba6b4146e26d913a0
notkath <148864502+notkath@users.noreply.github.com>
2024-08-04 14:39:03 +0530

    week2

---
+++ StringTokenizer2.java
@@ -0,0 +1,12 @@
+import java.util.StringTokenizer;
+public class StringTokenizer2
+{
+public static void main(String args[])
+{
+
+StringTokenizer st = new StringTokenizer("My name is Catherine");
+System.out.println("The total number of Tokens is:"+st.countTokens());
+
+
+}
+}
