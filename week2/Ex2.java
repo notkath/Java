b186c5866bd44dd38b539db615724771200a680f
notkath <148864502+notkath@users.noreply.github.com>
2024-08-04 15:26:23 +0530

    Add files via upload

---
+++ week2/Ex2.java
@@ -0,0 +1,22 @@
+class Student{
+public int roll_no;
+public String name;
+Student(int roll_no,String name)
+{
+this.roll_no=roll_no;
+this.name=name;
+}
+}
+
+public class Ex2{
+public static void main(String args[])
+{
+Student arr[];
+arr = new Student[5];
+arr[0] = Student(1,"aman");
+arr[1] = Student(2,"ben");
+arr[2] = Student(3,"ann");
+arr[3] = Student(4,"alan");
+arr[4] = Student(5,"swathi");
+}
+}
---
+++ week2/array(1).java
@@ -0,0 +1,14 @@
+public class array
+{
+public static void main(String[] arg)
+{
+for (int i=0;i<10;i++)
+{
+if(i==4)
+{
+continue;
+}
+System.out.println(i);
+}
+}
+}
